e40bd3e220269e3093c4f8a9cc67885b1a86973a
Toni Hoffmann <toni-hoffmann@xremix.de>
2015-11-24 21:40:48 +0100

    Test Temptask

--- app/index.js
+++ app/index.js
@@ -55,7 +55,7 @@ module.exports = generators.Base.extend({
 				version: this.inputProjectVersion
 			}
 			);
-		this.template('_Gulpfile.js', 'Gulpfile.js');
+		this.template('temptask.js', 'Gulpfile.js');
 
 	},
 	dependencies: function () {
